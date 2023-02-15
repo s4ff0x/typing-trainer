7d7dc02bcb6b2d3b3d9e826181ae515b085c3982
Anton Mendelson <mendelson.inbox@gmail.com>
2023-02-15 18:38:00 +0200

    feat: add fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['"JetBrains Mono"', 'monospace'],
+      mono: ['"JetBrains Mono"', 'monospace']
+    },
   },
   plugins: [],
 };
